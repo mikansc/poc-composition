423e2e2ad4db7899da6d72bbde1d03efc826def7
Michael Nascimento <michael.nsc@outlook.com>
2023-08-15 20:11:33 -0300

    feat: add new ComposedTabGroup

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,28 @@
+/* eslint-disable react/prop-types */
+import { FiAirplay, FiAnchor } from "react-icons/fi";
+import { ComposedTabGroup } from "./components/ComposedTabGroup/ComposedTabGroup";
+
+const TabContent = ({ children, totalTabs }) => {
+  return (
+    <div>
+      {totalTabs} tabs disponíveis
+      <h1>{children}</h1>
+    </div>
+  );
+};
+
 const App = () => {
   return (
     <div>
       <h1>Página inicial</h1>
+      <ComposedTabGroup>
+        <TabContent title="Detalhes" icon={FiAnchor} Icon={<FiAirplay />}>
+          Conteudo Tab 1
+        </TabContent>
+        <TabContent title="Dados técnicos">Conteudo Tab 2</TabContent>
+        <TabContent title="Comentários">Conteudo Tab 3</TabContent>
+        <TabContent title="Sei lá">Conteudo Tab 4</TabContent>
+      </ComposedTabGroup>
     </div>
   );
 };
