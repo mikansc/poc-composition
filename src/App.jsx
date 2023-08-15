2c146f4b04b3310844cc5608173b53ad3a83877c
Michael Nascimento <michael.nsc@outlook.com>
2023-08-15 18:59:08 -0300

    chore: add example files

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,7 @@
-import { Form } from "./components/Form";
-import { TabGroup } from "./components/TabGroup";
-
 const App = () => {
   return (
     <div>
-      <TabGroup />
-      <Form>
-        <Form.FieldGroup>
-          <Form.Label>First Name</Form.Label>
-          <Form.Input type="text" />
-        </Form.FieldGroup>
-        <Form.FieldGroup>
-          <Form.Label>Last Name</Form.Label>
-          <Form.Input type="text" />
-        </Form.FieldGroup>
-        <Form.FieldGroup>
-          <Form.Label>Email</Form.Label>
-          <Form.Input type="email" />
-        </Form.FieldGroup>
-        <Form.FieldGroup>
-          <Form.Label>Sex</Form.Label>
-          <Form.Select>
-            <option value="0">M</option>
-            <option value="1">F</option>
-          </Form.Select>
-        </Form.FieldGroup>
-        <Form.Action>
-          <button type="submit">Submit</button>
-        </Form.Action>
-      </Form>
+      <h1>Página inicial</h1>
     </div>
   );
 };
