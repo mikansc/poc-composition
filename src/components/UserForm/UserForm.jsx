815f98dfcc7684c20887d0297424509b3d296f73
Michael Nascimento <michael.nsc@outlook.com>
2023-08-15 20:05:00 -0300

    feat: user form

--- src/components/UserForm/UserForm.jsx
+++ src/components/UserForm/UserForm.jsx
@@ -1,9 +1,30 @@
-import styles from "./UserForm.module.scss";
+import { Form } from "../Form";
 
 export const UserForm = () => {
-    return (
-        <div className={styles.container}>
-            Componente UserForm
-        </div>
-    );
+  return (
+    <Form>
+      <Form.FieldGroup>
+        <Form.Label>First Name</Form.Label>
+        <Form.Input type="text" />
+      </Form.FieldGroup>
+      <Form.FieldGroup>
+        <Form.Label>Last Name</Form.Label>
+        <Form.Input type="text" />
+      </Form.FieldGroup>
+      <Form.FieldGroup>
+        <Form.Label>Email</Form.Label>
+        <Form.Input type="email" />
+      </Form.FieldGroup>
+      <Form.FieldGroup>
+        <Form.Label>Sex</Form.Label>
+        <Form.Select>
+          <option value="0">M</option>
+          <option value="1">F</option>
+        </Form.Select>
+      </Form.FieldGroup>
+      <Form.Action>
+        <button type="submit">Submit</button>
+      </Form.Action>
+    </Form>
+  );
 };
