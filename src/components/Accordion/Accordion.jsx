f2dd8a90680e8d2308f99967a632e1e0d4f321d3
Michael Nascimento <michael.nsc@outlook.com>
2023-08-15 21:11:17 -0300

    feat: Accordion exercise

--- src/components/Accordion/Accordion.jsx
+++ src/components/Accordion/Accordion.jsx
@@ -1,9 +1,15 @@
 import styles from "./Accordion.module.scss";
 
+/**
+ * EXERCÍCIO DE ACCORDION
+ *
+ * - Este componente deve exibir seus filhos como conteúdo de um accordion.
+ * - Deverá exibir um elemento visual com um título que, quando clicado, exibe ou esconde o conteúdo.
+ * - Pode receber uma prop "multiple" que indica se deve permitir que mais de um item esteja aberto ao mesmo tempo.
+ *
+ *   Bom trabalho!
+ */
+
 export const Accordion = () => {
-    return (
-        <div className={styles.container}>
-            Componente Accordion
-        </div>
-    );
+  return <div className={styles.container}>Componente Accordion</div>;
 };
