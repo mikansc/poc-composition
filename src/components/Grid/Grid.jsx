501f9e6b8ab0490cf3bc207c4a4612778cf36cec
Michael Nascimento <michael.nsc@outlook.com>
2023-08-15 21:09:35 -0300

    feat: grid exercise

--- src/components/Grid/Grid.jsx
+++ src/components/Grid/Grid.jsx
@@ -1,6 +1,13 @@
 /* eslint-disable react/prop-types */
 import styles from "./Grid.module.scss";
 
+/**
+ *  Este componente pode ser uma abstração de um grid system.
+ *
+ * Você pode implementar o display: grid pra exibir qualquer elemento filho
+ * em uma estrutura de grid!
+ */
+
 export const Grid = ({ children }) => {
   return <section className={styles.container}>{children}</section>;
 };
